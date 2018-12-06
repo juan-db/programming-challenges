9508cc0b4c820a659ba58fd2160b664e9f4fed3f
Juan de Bruin <juandb@pm.me>
2018-12-06 18:22:21 +0200

    Draw the snake on the screen

--- snake/source/app.d
+++ snake/source/app.d
@@ -1,7 +1,26 @@
 void main()
 {
+	/* Initialize curses. */
 	import nice.curses;
-	
 	auto curses = new Curses(Curses.Config());
-	curses.stdscr.box(0, 0);
+
+	import core.time;
+	import snake : Snake;
+
+	int lastY = 0;
+	auto lastUpdate = MonoTime.currTime;
+	auto timePerUpdate = dur!"msecs"(250);
+	while (true)
+	{
+		if (MonoTime.currTime - lastUpdate >= timePerUpdate)
+		{
+			lastUpdate = MonoTime.currTime;
+			curses.stdscr.clear();
+			auto player = new Snake(3, lastY++);
+			player.draw(curses);
+			curses.stdscr.box(0, 0);
+			curses.stdscr.refresh();
+			curses.update();
+		}
+	}
 }
