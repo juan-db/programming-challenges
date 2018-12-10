be42663678aec69b65fa6f08bb1b15adf3ce4e49
Juan de Bruin <juandb@pm.me>
2018-12-10 03:30:04 +0200

    Display entries on the screen

--- todo/source/app.d
+++ todo/source/app.d
@@ -2,7 +2,8 @@ import std.stdio : writeln;
 import deimos.ncurses;
 import std.file;
 import std.json;
-import std.string : indexOf;
+import std.string : indexOf, toStringz;
+import std.array : replicate;
 import todo_entry;
 
 int main(string[] args)
@@ -15,21 +16,35 @@ int main(string[] args)
 
 	/* read todo file. */
 	auto filename = args[1];
-	if (!exists(filename))
-	{
-		writeln("File \"", filename, "\" does not exist.");
-		return 2;
-	}
-	if (!isFile(filename))
-	{
-		writeln(filename, " is not a file.");
-		return 3;
-	}
-	auto json = parseJSON(readText(filename));
 	TodoEntry[] entries;
-	foreach (entry; json.array)
+	if (exists(filename))
 	{
-		entries ~= TodoEntry.fromJSON(entry);
+		if (!isFile(filename))
+		{
+			writeln("\"", filename, "\" exists but is not a file.");
+			return 2;
+		}
+		else
+		{
+			try
+			{
+				auto json = parseJSON(readText(filename));
+				foreach (entry; json.array)
+				{
+					entries ~= TodoEntry.fromJSON(entry);
+				}
+			}
+			catch (FileException fe)
+			{
+				writeln(fe.msg);
+				return 3;
+			}
+			catch (JSONException je)
+			{
+				writeln(je.msg);
+				return 4;
+			}
+		}
 	}
 
 	/* ncurses initialization. */
@@ -43,9 +58,55 @@ int main(string[] args)
 	set_escdelay(0);
 
 	/* Logic */
+	drawEntries(entries);
 	for (int ch; indexOf("qQ\x1b", ch = getch()) == -1;) // loop till 'q', 'Q', or escape
 	{
-
+		if (ch != ERR)
+		{
+			drawEntries(entries);
+		}
 	}
 	return 0;
+}
+
+private void drawEntries(TodoEntry[] entries)
+{
+	if (COLS == 0)
+	{
+		return;
+	}
+
+	void drawEntries(int indent, TodoEntry[] entries)
+	{
+		foreach (entry; entries)
+		{
+			auto note = replicate(" ", indent) ~ entry.getNote();
+			if (note.length > COLS)
+			{
+				if (COLS <= 3)
+				{
+					attron(A_UNDERLINE);
+					addstr(toStringz([".", "..", "..."][COLS - 1]));
+					attroff(A_UNDERLINE);
+					addch('\n');
+				}
+				else
+				{
+					addstr(toStringz(note[0..(COLS - 3)]));
+					attron(A_UNDERLINE);
+					addstr(toStringz("..."));
+					attroff(A_UNDERLINE);
+					addch('\n');
+				}
+			}
+			else
+			{
+				addstr(toStringz(note));
+				addch('\n');
+			}
+			drawEntries(indent + 2, entry.getChildren());
+		}
+	}
+
+	drawEntries(0, entries);
 }
