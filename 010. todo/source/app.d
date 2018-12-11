81adac2cd011526bfa7ddcd7b2f34e25a6714bcd
Juan de Bruin <juandb@pm.me>
2018-12-11 22:13:47 +0200

    Extract loading entries into separate function

--- 010. todo/source/app.d
+++ 010. todo/source/app.d
@@ -2,8 +2,9 @@ import std.stdio : writeln;
 import deimos.ncurses;
 import std.file;
 import std.json;
-import std.string : indexOf, toStringz;
+import std.string : indexOf, toStringz, fromStringz;
 import std.array : replicate;
+import std.conv : to;
 import todo_entry;
 
 int main(string[] args)
@@ -16,40 +17,25 @@ int main(string[] args)
 
 	/* read todo file. */
 	auto filename = args[1];
-	TodoEntry[] entries;
-	if (exists(filename))
+	TodoEntry[] entries = loadEntries(filename);
+	if (entries is null)
 	{
-		if (!isFile(filename))
-		{
-			writeln("\"", filename, "\" exists but is not a file.");
-			return 2;
-		}
-		else
-		{
-			try
-			{
-				auto json = parseJSON(readText(filename));
-				foreach (entry; json.array)
-				{
-					entries ~= TodoEntry.fromJSON(entry);
-				}
-			}
-			catch (FileException fe)
-			{
-				writeln(fe.msg);
-				return 3;
-			}
-			catch (JSONException je)
-			{
-				writeln(je.msg);
-				return 4;
-			}
-		}
+		return 2;
 	}
 
 	/* ncurses initialization. */
 	initscr();
 	scope(exit) endwin();
+	if (COLS <= 6)
+	{
+		writeln("Terminal must be more than 6 columns wide.");
+		return 5;
+	}
+	if (LINES <= 6)
+	{
+		writeln("Terminal must be more than 6 lines long.");
+		return 6;
+	}
 	cbreak();
 	noecho();
 	nonl();
@@ -57,25 +43,81 @@ int main(string[] args)
 	nodelay(stdscr, true);
 	set_escdelay(0);
 
-	/* Logic */
 	drawEntries(entries);
 	for (int ch; indexOf("qQ\x1b", ch = getch()) == -1;) // loop till 'q', 'Q', or escape
 	{
 		if (ch != ERR)
 		{
+			if (ch == KEY_F(2))
+			{
+				auto newEntry = createEntry();
+				entries ~= newEntry;
+			}
+			erase();
 			drawEntries(entries);
 		}
 	}
 	return 0;
 }
 
-private void drawEntries(TodoEntry[] entries)
+private TodoEntry[] loadEntries(string filename)
 {
-	if (COLS <= 3)
+	TodoEntry[] output;
+
+	if (!exists(filename))
 	{
-		return;
+		return output;
 	}
 
+	if (!isFile(filename))
+	{
+		writeln("\"", filename, "\" exists but is not a file.");
+		return null;
+	}
+
+	try
+	{
+		auto json = parseJSON(readText(filename));
+		foreach (entry; json.array)
+		{
+			output ~= TodoEntry.fromJSON(entry);
+		}
+	}
+	catch (FileException fe)
+	{
+		writeln(fe.msg);
+		return null;
+	}
+	catch (JSONException je)
+	{
+		writeln(je.msg);
+		return null;
+	}
+	return output;
+}
+
+private TodoEntry createEntry()
+{
+	erase();
+	attron(A_UNDERLINE);
+	addstr(toStringz("Create a new entry:"));
+	attroff(A_UNDERLINE);
+	addch(' ');
+	echo();
+	nocbreak();
+	nodelay(stdscr, false);
+	byte[256] buff;
+	getnstr(cast(char*)buff, 256);
+	auto str = fromStringz(cast(char*)buff);
+	auto output = new TodoEntry(cast(string)str);
+	noecho();
+	cbreak();
+	nodelay(stdscr, true);
+	return output;
+}
+
+private void drawEntries(TodoEntry[] entries)
+{
 	void drawEntries(int indent, TodoEntry[] entries)
 	{
 		foreach (entry; entries)
